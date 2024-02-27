40e30028c3d5f6e7adf6908f9f4a32a2ff0e9d24
Petru.Sacaleanu <Petru.Sacaleanu@GB-PF37STQX>
2024-02-27 15:19:18 +0000

    Removed unused dependecies.

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -1,37 +1,29 @@
-# test_tf_import.py
-import tensorflow as tf
-import pandas as pd
-from sklearn.model_selection import train_test_split
 #from tensorflow.keras.models import Sequential
 #from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-
 from tensorflow import keras
 from keras.models import Model
 from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 
-from keras.layers import Dense
-from keras.layers import Conv2D
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.layers import MaxPooling2D
 
-def create_cnn_model(input_shape):
+def create_cnn_model(input_shape, pool_size=(2, 2)):
     """
     Creates a CNN model with the specified input shape.
     """
     model = Sequential([
+
         Conv2D(24, (5, 5), activation='relu', input_shape=input_shape),
-        MaxPooling2D(pool_size=(2, 2)),
+        MaxPooling2D(pool_size),
         
         Conv2D(36, (5, 5), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
+        MaxPooling2D(),
         
         Conv2D(48, (5, 5), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
+        MaxPooling2D(pool_size),
         
         Conv2D(64, (3, 3), activation='relu'),
         Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
+        MaxPooling2D(pool_size),
         
         Flatten(),
         
@@ -42,8 +34,8 @@ def create_cnn_model(input_shape):
         Dense(10, activation='relu'),
         Dense(2)  # Output layer with 2 neurons for regression tasks
     ])
-    
-    # Compile the model with mean squared error loss and an optimizer of choice
-    model.compile(optimizer='adam', loss='mse')
+
+      # Compile the model with mean squared error loss and an optimizer of choice
+    model.compile(optimizer='adam', loss='mse')  
     
     return model
