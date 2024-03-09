acb61c1a31df7b946177977d8b29e41470971581
Petru.Sacaleanu <Petru.Sacaleanu@GB-PF37STQX>
2024-03-09 11:46:31 +0000

    New models

--- train_conv_CNN.py
+++ train_conv_CNN.py
@@ -7,6 +7,11 @@ from models.cnn_model import create_cnn_model, create_cnn_model_v2
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.losses import BinaryCrossentropy, MeanSquaredError
+from tensorflow.keras.optimizers import Adadelta
+import numpy as np
+
 
 
 
@@ -47,7 +52,7 @@ def train_cnn_model(X_train, y_train, epochs=100, batch_size=128, pool_size=(2, 
     input_shape = X_train.shape[1:]
     
     #model = create_cnn_model(input_shape, pool_size)
-    model = create_cnn_model_v2(input_shape, pool_size)
+    model = create_cnn_model(input_shape, pool_size)
     
     datagen = ImageDataGenerator(channel_shift_range=0.2)
     datagen.fit(X_train)
@@ -66,6 +71,98 @@ def train_cnn_model(X_train, y_train, epochs=100, batch_size=128, pool_size=(2, 
     
     return model, history
 
+
+def train_cnn_model_v2(X_train, y_train, epochs=100, batch_size=128, pool_size=(2, 2)):
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25)
+    
+    input_shape = X_train.shape[1:]
+    
+    # Create both models
+    model1, model2 = create_cnn_model_v2(input_shape, pool_size)
+    
+    datagen = ImageDataGenerator(channel_shift_range=0.2)
+    datagen.fit(X_train)
+    
+    # Use Adadelta optimizer with ExponentialDecay
+    initial_learning_rate = 1.0
+    decay_steps = 10000
+    decay_rate = 0.95
+    lr_schedule = ExponentialDecay(
+        initial_learning_rate, decay_steps, decay_rate, staircase=True
+    )
+    
+    # Use Adadelta optimizer for both models
+    optimizer = Adadelta(learning_rate=lr_schedule)
+
+       # Compile each model separately
+    model1.compile(
+        optimizer=optimizer,
+        loss={
+            'output_speed': BinaryCrossentropy(from_logits=False),
+        },
+        loss_weights={
+            'output_speed': 1,
+        },
+        weighted_metrics=[]
+    )
+
+    model2.compile(
+        optimizer=optimizer,
+        loss={
+            'output_angle': MeanSquaredError(),
+        },
+        loss_weights={
+            'output_angle': 1,
+        },
+        weighted_metrics=[]
+    )
+
+    # Ensure y_train has the correct shape
+    y_train = np.column_stack((y_train[:, 0], y_train[:, 1]))
+    print(y_train.shape)
+    print(X_train.shape)
+    # Fit each model separately
+    history1 = model1.fit_generator(        datagen.flow(X_train, {'output_speed': y_train[:, 1]}, batch_size=batch_size),
+        steps_per_epoch=len(X_train)//batch_size,
+        epochs=epochs,
+        verbose=1,
+        validation_data=(datagen.standardize(X_val), {'output_speed': y_val[:, 1]})
+    )
+    
+    history2 = model2.fit_generator(
+        datagen.flow(X_train, {'output_angle': y_train[:, 0]}, batch_size=batch_size),
+        steps_per_epoch=len(X_train)//batch_size,
+        epochs=epochs,
+        verbose=1,
+        validation_data=(datagen.standardize(X_val), {'output_angle': y_val[:, 0]})
+    )
+
+   # history1 = model1.fit_generator(datagen.flow(X_train, {'output_speed': y_train[:, 1]}, batch_size=batch_size),
+   #                                 steps_per_epoch=len(X_train)//batch_size,
+   #                                 epochs=epochs,
+   #                                 verbose=1,
+   #                                 validation_data=(X_val, {'output_speed': y_val[:, 1]}))
+   # 
+   # history2 = model2.fit_generator(datagen.flow(X_train, {'output_angle': y_train[:, 0]}, batch_size=batch_size),
+   #                                 steps_per_epoch=len(X_train)//batch_size,
+   #                                 epochs=epochs,
+   #                                 verbose=1,
+   #                                 validation_data=(X_val, {'output_angle': y_val[:, 0]}))
+   # 
+
+  
+
+    model1.trainable = False
+    model2.trainable = False
+
+    model1.save('full_CNN_model_speed.h5')
+    model2.save('full_CNN_model_angle.h5')
+
+    model1.summary()
+    model2.summary()
+
+    return model1, history1, model2, history2
+
 def plot_loss(history):
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -77,6 +174,9 @@ def plot_loss(history):
 
 if __name__ == "__main__":
     training_images, labels = load_and_preprocess_data()
-    model, training_history = train_cnn_model(training_images, labels)
-    test_cnn_model(model)
-    plot_loss(training_history)
+    print(labels)
+    model1, history1, model2, history2 = train_cnn_model_v2(training_images, labels)
+    test_cnn_model(model1)
+    test_cnn_model(model2)
+    plot_loss(history1)
+    plot_loss(history2)
