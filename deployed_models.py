3ebb20a44764b2461789c195b9a32dbbab7c3190
Petru.Sacaleanu <Petru.Sacaleanu@GB-PF37STQX>
2024-04-22 16:29:25 +0100

    Deploy models onto the car.

---
+++ deployed_models.py
@@ -0,0 +1,22 @@
+
+
+
+# Implement the code to deploy our models.
+# model is :03-24_15-47_CNN_model_speed_epochs1050.h5   and 03-27_22-06_CNN_model_angle_epochs3000.h5
+# 
+
+# Info for deploying our code : Code Repository
+'''
+https://moodle.nottingham.ac.uk/pluginfile.php/9815695/mod_resource/content/5/MLiS_Project_Guidance_2023%20%282%29.pdf
+The remote control repository can be found at https://github.com/adammoss/SunFounder_PiCar-V and is located on your RPi at
+/home/pi/SunFounder PiCar-V/remote control. This has been cloned and adapted
+from SunFounder for this project by the module conveners. You should not modify
+the master branch of this code, as this can cause merge conflicts if we update it. Instead, you will develop an installable package to drive the car using deep learning,
+according to the API specifications below, and only set environment variables where
+needed.
+If major changes are made we will inform you, and these can be merged by
+opening a terminal and entering git pull origin master within the repository. If
+you would like to add features that you think would be beneficial, this should be done
+by submitting a pull request. An example would be an improved UI to manually
+control the car. If approved, we can then merge this into the master branch of the
+repository and everyone will benefit. '''#
