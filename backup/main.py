516ca8dd84b3b1f078f1bbe50f1a0e1d582ac7df
razvanpetru1 <razvan.petru1@gmail.com>
2024-05-05 23:46:38 +0100

    Removed unused files.

--- backup/main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-import train
-from utils.preprocessing import load_and_preprocess_images
-from models.cnn_model import create_cnn_model
-
-# Call the training function
-if __name__ == '__main__':
-    train.train_model()
